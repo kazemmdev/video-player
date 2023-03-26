76606eb3d00773c04352e2f9cb121f517dd4bbec
Kazem Mirzaei <k90mirzaei@gmail.com>
2023-03-26 12:08:44 +0330

    chore: add an example project

---
+++ example/src/App.tsx
@@ -0,0 +1,11 @@
+import { VideoPlayer } from "@k90mirzaei/video-player";
+
+function App() {
+  return (
+    <div className="App">
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+import postcss from "rollup-plugin-postcss";
 const packageJson = require("./package.json");
 
 export default [
@@ -24,11 +25,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts.default()],
+    external: [/\.css$/],
   },
 ];
